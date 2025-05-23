3c261931af1c3c68b63ea39aee339d0d8dfd66cf
Dmitry Alekseevich <opolindmitry@gmail.com>
2025-05-23 20:13:44 +0300

    add reviewer change

--- llvm/compiler-course/backend/opolin_d_lab3_var1/ExamplePass.cpp
+++ llvm/compiler-course/backend/opolin_d_lab3_var1/ExamplePass.cpp
@@ -14,17 +14,7 @@ namespace {
 class AVXLogicCombinerPass : public MachineFunctionPass {
   const X86InstrInfo *TII = nullptr;
   MachineRegisterInfo *RegInfo = nullptr;
-  DenseMap<unsigned, unsigned> scalarToAVX;
-
-  void initMap() {
-    scalarToAVX = {
-        {X86::PANDrr, X86::VPANDrr},   {X86::PORrr, X86::VPORrr},
-        {X86::PXORrr, X86::VPXORrr},   {X86::PANDNrr, X86::VPANDNrr},
-        {X86::ANDPSrr, X86::VANDPSrr}, {X86::ORPSrr, X86::VORPSrr},
-        {X86::XORPSrr, X86::VXORPSrr}, {X86::ANDPDrr, X86::VANDPDrr},
-        {X86::ORPDrr, X86::VORPDrr},   {X86::XORPDrr, X86::VXORPDrr},
-    };
-  }
+  static const DenseMap<unsigned, unsigned> scalarToAVX;
 
   bool tryFoldPair(MachineBasicBlock &MBB, MachineBasicBlock::iterator &it) {
     MachineInstr &curr = *it;
@@ -52,6 +42,22 @@ class AVXLogicCombinerPass : public MachineFunctionPass {
   void fuseInstructions(MachineBasicBlock &MBB, MachineBasicBlock::iterator &it,
                         MachineInstr *defMI, unsigned opc1, MachineInstr &useMI,
                         unsigned opc2) {
+    Register def1 = defMI->getOperand(1).getReg();
+    Register def2 = defMI->getOperand(2).getReg();
+    Register useDest = useMI.getOperand(0).getReg();
+    Register useSrc = useMI.getOperand(2).getReg();
+
+    bool isOR = (opc1 == X86::PORrr || opc1 == X86::ORPSrr ||
+                 opc1 == X86::ORPDrr);
+    bool isAND = (opc2 == X86::PANDrr || opc2 == X86::ANDPSrr ||
+                  opc2 == X86::ANDPDrr);
+    bool match = (useSrc == def1) || (useSrc == def2);
+    if (isOR && isAND && match) {
+      Register toCopy = (useSrc == def1) ? def1 : def2; 
+      BuildMI(MBB, it, useMI.getDebugLoc(), TII->get(X86::MOVAPSrr), useDest)
+          .addReg(toCopy);
+      return;
+    }
     Register tmp = RegInfo->createVirtualRegister(
         RegInfo->getRegClass(useMI.getOperand(1).getReg()));
     DebugLoc dlDef = defMI->getDebugLoc();
@@ -85,7 +91,7 @@ class AVXLogicCombinerPass : public MachineFunctionPass {
 
 public:
   static char ID;
-  AVXLogicCombinerPass() : MachineFunctionPass(ID) { initMap(); }
+  AVXLogicCombinerPass() : MachineFunctionPass(ID) {}
   bool runOnMachineFunction(MachineFunction &MF) override {
     const X86Subtarget &ST = MF.getSubtarget<X86Subtarget>();
     TII = ST.getInstrInfo();
@@ -106,6 +112,14 @@ public:
     }
     return Changed;
   }
+};
+
+const DenseMap<unsigned, unsigned> AVXLogicCombinerPass::scalarToAVX = {
+    {X86::PANDrr, X86::VPANDrr},   {X86::PORrr, X86::VPORrr},
+    {X86::PXORrr, X86::VPXORrr},   {X86::PANDNrr, X86::VPANDNrr},
+    {X86::ANDPSrr, X86::VANDPSrr}, {X86::ORPSrr, X86::VORPSrr},
+    {X86::XORPSrr, X86::VXORPSrr}, {X86::ANDPDrr, X86::VANDPDrr},
+    {X86::ORPDrr, X86::VORPDrr},   {X86::XORPDrr, X86::VXORPDrr},
 };
 
 char AVXLogicCombinerPass::ID = 0;
